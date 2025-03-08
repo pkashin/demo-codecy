56830174ada46dea432ff81ffa521d575f292138
pkashin <48813179+pkashin@users.noreply.github.com>
2025-03-08 17:13:24 +0700

    commit

--- util_test.go
+++ util_test.go
@@ -5,6 +5,30 @@ import (
 	"testing"
 )
 
+func BenchmarkExample(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+	}
+}
+
+func BenchmarkFib10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var _ = Fib(10)
+	}
+}
+
+func BenchmarkFib20(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var _ = Fib(20)
+	}
+}
+
+func BenchmarkFib20WithAuxMetric(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var _ = Fib(20)
+	}
+	b.ReportMetric(4.0, "auxMetricUnits")
+}
+
 func TestFormatPath(t *testing.T) {
 	tests := []struct {
 		name     string
